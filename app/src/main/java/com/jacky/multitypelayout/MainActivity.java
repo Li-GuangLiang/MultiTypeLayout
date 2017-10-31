839b850bd0a1fd973a3a2631064382bd5f4566e4
Jacky <1028964216@qq.com>
2017-10-31 13:58:51 +0800

    测试提交

--- app/src/main/java/com/jacky/multitypelayout/MainActivity.java
+++ app/src/main/java/com/jacky/multitypelayout/MainActivity.java
@@ -1,7 +1,8 @@
 package com.jacky.multitypelayout;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("tag","李光亮提交");
     }
 
 }
